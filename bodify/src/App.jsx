0155f2bde156ffd288c6de1ae7e7fb8bc0b8b70c
huongtra01 <133492916+huongtra01@users.noreply.github.com>
2025-04-06 19:54:15 +0700

    add chatbox ui

--- bodify/src/App.jsx
+++ bodify/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes';
+import Chatbox from './components/Chatbox';
 
 function App() {
   return (
-    <RouterProvider router={routes} />
+    <>
+      <RouterProvider router={routes} />
+      <Chatbox />
+    </>
   );
 }
 
